c42184db505f7bb9c5a6c5dba93aff6a7c10daaa
BlinksCrib <ajibolaisaac@gmail.com>
2022-05-30 16:32:09 +0100

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState} from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import images from "../../constants/images";
+import {images} from "../../constants/images";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -10,7 +10,7 @@ const Navbar = () => {
 return( 
   <nav className="app__navbar">
     <div className="app__navbar-logo">
-      <img src={images.gericht} alt="app logo" />
+      <img src={images.gerich} alt="app logo" />
     </div>
     <ul className="app__navbar-links">
       <li className="p__opensans">
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,47 +1,69 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
+import bgt from "../assets/bg.png";
+import cheft from "../assets/chef.png";
+import Gt from "../assets/G.png";
+import gallery01t from "../assets/gallery01.png";
+import gallery02t from "../assets/gallery02.png";
+import gallery03t from "../assets/gallery03.png";
+import gallery04t from "../assets/gallery04.png";
+import knifet from "../assets/knife.png";
+import logot from "../assets/logo.png";
+import menut from "../assets/menu.png";
+import overlaybgt from "../assets/overlaybg.png";
+import spoont from "../assets/spoon.svg";
+import welcomet from "../assets/welcome.png";
+import findust from "../assets/findus.png";
+import laurelst from "../assets/laurels.png";
+import award01t from "../assets/award01.png";
+import award02t from "../assets/award02.png";
+import award03t from "../assets/award03.png";
+import award05t from "../assets/award05.png";
+import signt from "../assets/sign.png";
+import quotet from "../assets/quote.png";
+import gericht from "../assets/gericht.png";
 
-export default {
-  bg,
-  chef,
-  G,
-  gallery01,
-  gallery02,
-  gallery03,
-  gallery04,
-  knife,
-  logo,
-  menu,
-  overlaybg,
-  spoon,
-  welcome,
-  findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
-  quote,
-  gericht,
+export const images = {
+  bg: bgt,
+  gerich: gericht,
+  chef: cheft,
+  G: Gt,
+  gallery01: gallery01t,
+  gallery02: gallery02t,
+  gallery03: gallery03t,
+  gallery04t: gallery04t,
+  knife: knifet,
+  logo: logot,
+  menu: menut,
+  overlaybg: overlaybgt,
+  spoon: spoont,
+  welcome: welcomet,
+  findus: findust,
+  laurels: laurelst,
+  award01: award01t,
+  award02: award02t,
+  award03: award03t,
+  award05: award05t,
+  sign: signt,
+  quote: quotet,
 };
+
+// {},
+// {chef},
+// {G},
+// {gallery01},
+// {gallery02},
+// {gallery03},
+// {gallery04},
+// {knife},
+// {logo},
+// {menu},
+// {overlaybg},
+// {spoon},
+// {welcome},
+// {findus},
+// {laurels},
+// {award01},
+// {award02},
+// {award03},
+// {award05},
+// {sign},
+// {quote},
