db50a6f50f70224f796d8bc0b681e5fa7b8a6cc5
BlinksCrib <ajibolaisaac@gmail.com>
2022-05-31 10:46:12 +0100

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState} from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import {images} from "../../constants/images";
+import images from "../../constants/images";
 import "./Navbar.css";
 
 const Navbar = () => {
